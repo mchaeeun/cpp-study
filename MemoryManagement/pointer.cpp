3ee4e6d5693fbc54ad662ed1acc1a00cc465af93
mchaeeun <chaeeun@dgu.ac.kr>
2024-09-24 23:52:54 +0900

    [포인터 실습] 함수 포인터 - 함수 포인터 기본 개념 - 콜백 함수를 통한 동적 함수 처리

--- MemoryManagement/pointer.cpp
+++ MemoryManagement/pointer.cpp
@@ -87,6 +87,16 @@ public:
 	}
 };
 
+// 두 숫자를 더하는 함수
+int add(int a, int b) {
+	return a + b;
+}
+
+// 콜백 함수 선언
+void callBack(int x, int y, int (*callback)(int, int)) {
+	cout << "process: " << callback(x, y) << endl;
+}
+
 void typeofPointer() {
 	cout << " --포인터 유형-- " << endl;
 	// 1. void 포인터: 모든 데이터 유형을 가르킬 수 있는 포인터
@@ -94,7 +104,13 @@ void typeofPointer() {
 	string s = "Hello";
 	voidPtr = &s;
 	cout << "s: " << s << endl;
-	cout << "voidPtr: " << voidPtr << endl;
+	// cout << "voidPtr: " << *voidPtr << endl; // 오류: void 포인터는 역참조 불가
+	// voidPtr를 string*로 캐스팅 후 역참조
+	cout << "*voidPtr: " << *(static_cast<string*>(voidPtr)) << endl;
+	int ten = 10;
+	voidPtr = &ten;
+	cout << "ten: " << ten << endl;
+	cout << "*voidPtr: " << *(static_cast<int*>(voidPtr)) << endl;
 
 	// 2. nullptr: 포인터가 가르키는 주소가 없음을 나타내는 값
 	int* nullPtr = nullptr;
@@ -102,8 +118,8 @@ void typeofPointer() {
 
 	// 3. 동적 메모리 할당: 프로그램 실행 중에 메모리를 할당
 	int* dynamicPtr = new int; // int형 변수를 가르키는 포인터 dynamicPtr 선언
+	cout << "*dynamicPtr: " << *dynamicPtr << endl; // 쓰레기 값이 들어갈 수 있음
 	*dynamicPtr = 15; // dynamicPtr이 가르키는 주소에 15 저장
-	cout << "dynamicPtr: " << dynamicPtr << endl;
 	cout << "*dynamicPtr: " << *dynamicPtr << endl;
 	delete dynamicPtr; // 동적 메모리 해제
 
@@ -163,7 +179,9 @@ void typeofPointer() {
 	int f = 6;
 	int g = 7;
 	int& ref = f; // int형 변수 f를 참조하는 참조자 ref 선언
+	// int& ref2; // 오류: 참조자는 선언과 동시에 초기화해야 함
 	cout << "f: " << f << ", ref: " << ref << endl;
+	cout << "&f: " << &f << ", &ref: " << &ref << endl;
 	ref = 7; // ref가 가르키는 변수 f의 값 변경
 	cout << "f: " << f << ", ref: " << ref << endl;
 	f = 8; // f의 값 변경
@@ -172,4 +190,20 @@ void typeofPointer() {
 	ref = g; // ref이 g를 참조하도록 변경하는 게 아니라 ref가 가르키는 변수 값을 g로 변경
 	g = 9; // g의 값 변경
 	cout << "f: " << f << ", ref: " << ref << endl;
+
+	// 12. 함수 포인터: 함수의 주소를 저장하는 포인터
+	int (*funcPtr)(int, int); // int형 매개변수 두 개를 받아 int형 값을 반환하는 함수 포인터 선언
+	funcPtr = &add; // add 함수의 주소를 funcPtr에 저장
+	// 함수 이름은 해당 함수의 주소를 나타냄
+	// 따라서 add와 &add는 동일한 동작
+	funcPtr = add;
+	cout << "add(1, 2): " << funcPtr(1, 2) << endl; // funcPtr이 가르키는 함수 호출
+
+	// 함수	포인터 배열 선언
+	int (*funcPtrArr[2])(int, int) = { add, add }; // add 함수의 주소를 가지는 함수 포인터 배열 선언
+	cout << "add(3, 4): " << funcPtrArr[0](3, 4) << endl;
+	cout << "add(5, 6): " << funcPtrArr[1](5, 6) << endl;
+
+	// 콜백 함수
+	callBack(7, 8, add); // add 함수의 주소를 콜백 함수에 전달
 }
